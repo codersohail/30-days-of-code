1eec8e194aac6fa07801cad55a02792cb8ca6883
codersohail <107191800+codersohail@users.noreply.github.com>
2022-06-15 17:57:02 +0530

    Update and rename negative_oneside&positive_oneside.java to negative_oneside_positive_oneside.java

--- negative_oneside_positive_oneside.java
+++ negative_oneside_positive_oneside.java
@@ -1,13 +1,13 @@
 import java.util.*;
 public class jun15 {  
-    public static void neg&pos(int[] ary)
+    public static void neg_pos(int[] ary)
     {
     Arrays.sort(ary);
     }
     public static void main(String[] args)
     {
        int[] arr = {100,20,-6,18,-13,-15,20 };
-        neg&pos(ary);
+        neg_pos(ary);
       //i used foreach lop here because its easy for me
         for (int x : ary)
             System.out.print(x + " ");
