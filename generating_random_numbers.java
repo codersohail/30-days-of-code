0ba1b86daf96aa4f4c2deba161d66d17db31458f
codersohail <107191800+codersohail@users.noreply.github.com>
2022-06-21 19:09:30 +0530

    Create generating_random_numbers.java

---
+++ generating_random_numbers.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+public class jun21{
+public static void main(String args[])
+{
+
+Random r = new Random();
+
+int int1 = r.nextInt(30);
+int int2 = r.nextInt(6);
+
+System.out.println("Random Integers: "+int1);
+System.out.println("Random Integers: "+int2);
+
+}
+}
